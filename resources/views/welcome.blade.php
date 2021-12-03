b88b346de0957ea9cf943f89e10c049e39b66316
haoyu111 <72409343+haoyu111@users.noreply.github.com>
2021-12-03 02:59:06 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-  <title>@yield('title')</title>
+  <title>Page4b</title>
 
   <!-- Required meta tags -->
   <meta charset="utf-8">
@@ -11,35 +11,92 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
 
 </head>
+<style>
+	.box{
+		border:3px solid black;
+		margin:10px;
+		padding:15px;
+		
+	}
+	aside{
+		width: 30%;
+		float: right;
+		margin:10px;
+	}
+	#fix-size{
+		max-width:100%;
+		max-height:100%;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%;
+		display: block;
+	}		
+</style>
 <body>
 
   <!-- navigation bar -->
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-6 offset-sm-3 col-md-6 offset-md-3">
-        
-        <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <a class="nav-link" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/todo/active">Active</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/todo/done">Done</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/todo/deleted">Deleted</a>
-          </li>
-        </ul>
-        
-      </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#c">Canada</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#intro">Introduction <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#hist">History</a>
+      <a class="nav-item nav-link" href="#ety">Etymology</a>
+      <a class="nav-item nav-link" href="#climate">Climate</a>
     </div>
   </div>
+</nav>
   <!-- navigation bar ends here -->
+<h1 id="c" style="text-align:center;">Canada</h1>
+<div class="box">
+		<aside>
+			<figure>
+			<img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/4f/Coat_of_arms_of_Canada.svg/128px-Coat_of_arms_of_Canada.svg.png" id="fix-size">
+			<figcaption style="text-align:center;">Fig.1-Coat of Arms</figcaption>
+			</figure>
+		</aside>
+	<h2 id="intro">Introduction</h2>
+	<p>Canada is a country in North America. Its ten provinces and three territories extend from the Atlantic to the Pacific and northward into the Arctic Ocean, covering 9.98 million square kilometres (3.85 million square miles), making it the world's second-largest country by total area. Its southern and western border with the United States, stretching 8,891 kilometres (5,525 mi), is the world's longest bi-national land border. Canada's capital is Ottawa, and its three largest metropolitan areas are Toronto, Montreal, and Vancouver.</p>
+	<p>Indigenous peoples have continuously inhabited what is now Canada for thousands of years. Beginning in the 16th century, British and French expeditions explored and later settled along the Atlantic coast. As a consequence of various armed conflicts, France ceded nearly all of its colonies in North America in 1763. In 1867, with the union of three British North American colonies through Confederation, Canada was formed as a federal dominion of four provinces. This began an accretion of provinces and territories and a process of increasing autonomy from the United Kingdom. This widening autonomy was highlighted by the Statute of Westminster 1931 and culminated in the Canada Act 1982, which severed the vestiges of legal dependence on the Parliament of the United Kingdom.</p>
+	<p>Canada is a parliamentary democracy and a constitutional monarchy in the Westminster tradition. The country's head of government is the prime minister-who holds office by virtue of their ability to command the confidence of the elected House of Commons-and is appointed by the governor general, representing the monarch, who serves as head of state. The country is a Commonwealth realm and is officially bilingual at the federal level. It ranks among the highest in international measurements of government transparency, civil liberties, quality of life, economic freedom, and education. It is one of the world's most ethnically diverse and multicultural nations, the product of large-scale immigration from many other countries. Canada's long and complex relationship with the United States has had a significant impact on its economy and culture.</p>
+	<p>A highly developed country, Canada has the 26th highest nominal per-capita income globally and the sixteenth-highest ranking in the Human Development Index. Its advanced economy is the ninth-largest in the world, relying chiefly upon its abundant natural resources and well-developed international trade networks. Canada is part of several major international and intergovernmental institutions or groupings including the United Nations, NATO, the G7, the Group of Ten, the G20, the Organisation for Economic Co-operation and Development (OECD), the World Trade Organization (WTO), the Commonwealth of Nations, the Arctic Council, the Organisation internationale de la Francophonie, the Asia-Pacific Economic Cooperation forum, and the Organization of American States.</p>
+	
+		
+</div>
+<div class="box">
 
+		<h2 id="hist">Contemporary era history</h2>
+		<p>The financial crisis of the Great Depression had led the Dominion of Newfoundland to relinquish responsible government in 1934 and become a Crown colony ruled by a British governor. After two referendums, Newfoundlanders voted to join Canada in 1949 as a province.</p>
+		<p>Canada's post-war economic growth, combined with the policies of successive Liberal governments, led to the emergence of a new Canadian identity, marked by the adoption of the Maple Leaf Flag in 1965, the implementation of official bilingualism (English and French) in 1969, and the institution of official multiculturalism in 1971. Socially democratic programs were also instituted, such as Medicare, the Canada Pension Plan, and Canada Student Loans, though provincial governments, particularly Quebec and Alberta, opposed many of these as incursions into their jurisdictions.</p>
+		<p>Finally, another series of constitutional conferences resulted in the UK's Canada Act 1982, the patriation of Canada's constitution from the United Kingdom, concurrent with the creation of the Canadian Charter of Rights and Freedoms. Canada had established complete sovereignty as an independent country, although the monarch is retained as sovereign. In 1999, Nunavut became Canada's third territory after a series of negotiations with the federal government.</p>
+		<aside>
+			<figure>
+			<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Canadiancharterofrightsandfreedoms.jpg/330px-Canadiancharterofrightsandfreedoms.jpg" id="fix-size">
+			<figcaption style="text-align:center;">Fig.2-A copy of the Canadian Charter of Rights and Freedoms</figcaption>
+			</figure>
+		</aside>			
+		<p>At the same time, Quebec underwent profound social and economic changes through the Quiet Revolution of the 1960s, giving birth to a secular nationalist movement. The radical Front de liberation du Quebec (FLQ) ignited the October Crisis with a series of bombings and kidnappings in 1970 and the sovereignist Parti Quebecois was elected in 1976, organizing an unsuccessful referendum on sovereignty-association in 1980. Attempts to accommodate Quebec nationalism constitutionally through the Meech Lake Accord failed in 1990. This led to the formation of the Bloc Quebecois in Quebec and the invigoration of the Reform Party of Canada in the West. A second referendum followed in 1995, in which sovereignty was rejected by a slimmer margin of 50.6 to 49.4 percent. In 1997, the Supreme Court ruled unilateral secession by a province would be unconstitutional and the Clarity Act was passed by parliament, outlining the terms of a negotiated departure from Confederation.</p>
+		<p>In addition to the issues of Quebec sovereignty, a number of crises shook Canadian society in the late 1980s and early 1990s. These included the explosion of Air India Flight 182 in 1985, the largest mass murder in Canadian history; the Ecole Polytechnique massacre in 1989, a university shooting targeting female students; and the Oka Crisis of 1990, the first of a number of violent confrontations between the government and Indigenous groups. Canada also joined the Gulf War in 1990 as part of a United States-led coalition force and was active in several peacekeeping missions in the 1990s, including the UNPROFOR mission in the former Yugoslavia. Canada sent troops to Afghanistan in 2001 but declined to join the United States-led invasion of Iraq in 2003.</p>
+		<p>In 2011, Canadian forces participated in the NATO-led intervention into the Libyan Civil War, and also became involved in battling the Islamic State insurgency in Iraq in the mid-2010s.The COVID-19 pandemic in Canada began on January 27, 2020, with wide social and economic disruption. In 2021, the remains of hundreds of Indigenous people were discovered near the former sites of Canadian Indian residential schools. Administered by the Canadian Catholic Church and funded by the Canadian government from 1828 to 1997, these boarding schools attempted to assimilate Indigenous children into Euro-Canadian culture.</p>
+		
+</div>
 
-  @yield('content')
+<div class="box">
+	<h2 id="ety">Etymology</h2>
+	<p>While a variety of theories have been postulated for the etymological origins of Canada, the name is now accepted as coming from the St. Lawrence Iroquoian word kanata, meaning "village" or "settlement". In 1535, Indigenous inhabitants of the present-day Quebec City region used the word to direct French explorer Jacques Cartier to the village of Stadacona. Cartier later used the word Canada to refer not only to that particular village but to the entire area subject to Donnacona (the chief at Stadacona); by 1545, European books and maps had begun referring to this small region along the Saint Lawrence River as Canada.</p>
+	<p>From the 16th to the early 18th century, "Canada" referred to the part of New France that lay along the Saint Lawrence River. In 1791, the area became two British colonies called Upper Canada and Lower Canada. These two colonies were collectively named the Canadas until their union as the British Province of Canada in 1841.</p>
+	<p>Upon Confederation in 1867, Canada was adopted as the legal name for the new country at the London Conference, and the word Dominion was conferred as the country's title. By the 1950s, the term Dominion of Canada was no longer used by the United Kingdom, which considered Canada a "Realm of the Commonwealth". The government of Louis St. Laurent ended the practice of using Dominion in the statutes of Canada in 1951.</p>
+	<p>The Canada Act 1982, which brought the constitution of Canada fully under Canadian control, referred only to Canada. Later that year, the name of the national holiday was changed from Dominion Day to Canada Day. The term Dominion was used to distinguish the federal government from the provinces, though after the Second World War the term federal had replaced dominion.</p>
+</div>
+<div class="box">
+	<h2 id="climate">Climate</h2>
+	<p>Average winter and summer high temperatures across Canada vary from region to region. Winters can be harsh in many parts of the country, particularly in the interior and Prairie provinces, which experience a continental climate, where daily average temperatures are near -15 C (5 F), but can drop below -40 C (-40 F) with severe wind chills. In non-coastal regions, snow can cover the ground for almost six months of the year, while in parts of the north snow can persist year-round. Coastal British Columbia has a temperate climate, with a mild and rainy winter. On the east and west coasts, average high temperatures are generally in the low 20s C (70s F), while between the coasts, the average summer high temperature ranges from 25 to 30 C (77 to 86 F), with temperatures in some interior locations occasionally exceeding 40 C (104 F).</p>
+	<p>Much of Northern Canada is covered by ice and permafrost; however, the future of the permafrost is uncertain because the Arctic has been warming at three times the global average as a result of climate change in Canada. Canada's annual average temperature over land has warmed by 1.7 C (3.1 F), with changes ranging from 1.1 to 2.3 C (2.0 to 4.1 F) in various regions, since 1948. The rate of warming has been higher across the North and in the Prairies. In the southern regions of Canada, air pollution from both Canada and the United States-caused by metal smelting, burning coal to power utilities, and vehicle emissions-has resulted in acid rain, which has severely impacted waterways, forest growth and agricultural productivity in Canada.</p>
+</div>
+
 
 
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
